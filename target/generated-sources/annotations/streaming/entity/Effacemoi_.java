bf185f13ea5edb023d56892fbfee073e383ee054
jean <jeanmarie.projects@gmail.com>
2019-09-01 18:22:36 +0100

    Initialisation du projet

---
+++ target/generated-sources/annotations/streaming/entity/Effacemoi_.java
@@ -0,0 +1,13 @@
+package streaming.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-01T17:31:57")
+@StaticMetamodel(Effacemoi.class)
+public class Effacemoi_ { 
+
+    public static volatile SingularAttribute<Effacemoi, Long> id;
+
+}
